df434f443367f1c32b03a952a600bfcd9f92e955
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-14 18:06:37 +0100

    initial routing code

--- src/plugins/example/index.ts
+++ src/plugins/example/index.ts
@@ -1,6 +1,7 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  IRouter
 } from '@jupyterlab/application';
 
 import { DOMUtils, MainAreaWidget } from '@jupyterlab/apputils';
@@ -9,6 +10,7 @@ import { IMainMenu } from '../top/tokens';
 
 import { fileIcon, jupyterIcon } from '@jupyterlab/ui-components';
 
+// BoxLayout
 import { Widget } from '@lumino/widgets';
 
 /**
@@ -18,27 +20,76 @@ export namespace CommandIDs {
   export const open = 'jupyterlab-app-template:open';
 }
 
+class ViewCreator {
+  _parent: Widget;
+  constructor(parent: Widget) {
+    this._parent = parent;
+  }
+
+  createView(viewName: string, args: Array<string>) {
+    const node = document.createElement('a');
+
+    console.log("Navigating to ", viewName, " with args: ", args)
+
+    if (viewName == 'packages')
+    {
+      // create a packages view
+      node.textContent = 'Packages: Hello world ' + args;
+      node.href = "/example/quetz/channels";
+    }
+    else if (viewName == 'channels')
+    {
+      // create a channels view
+      node.textContent = 'Channels: Hello world ' + args;
+      node.href = "/example/quetz/packages";
+    }
+
+    this._parent.node.innerHTML = '';
+    this._parent.node.appendChild(node);
+  }
+}
+
+
 /**
  * The main plugin.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-app-template:main',
   autoStart: true,
+  requires: [IRouter],
   optional: [IMainMenu],
-  activate: (app: JupyterFrontEnd, menu: IMainMenu | null): void => {
+  activate: (app: JupyterFrontEnd, router: IRouter, menu: IMainMenu | null): void => {
     const { commands, shell } = app;
 
-    const node = document.createElement('div');
+    const node = document.createElement('a');
+
     node.textContent = 'Hello world!';
-    const content = new Widget({ node });
+    node.href = "/example/quetz/packages"
+
+    // let parent_layout = new BoxLayout();
+    let content = new Widget({ node });
     content.id = DOMUtils.createDomID();
     content.title.label = 'Hello';
     content.title.caption = 'Hello World';
     content.title.icon = fileIcon;
     content.addClass('jp-ExampleWidget');
+
+    // parent_layout.addWidget(content);
     const widget = new MainAreaWidget({ content });
     widget.title.closable = true;
     shell.add(widget, 'main');
+
+    let viewCreator = new ViewCreator(content);
+
+    commands.addCommand('quetz:gotopage', {
+      execute: () => {
+        let url = new URL(window.location.href)
+        let parts = url.pathname.split('/').slice(3);
+        viewCreator.createView(parts[0], parts.slice(1));
+      }
+    });
+
+    router.register({pattern: /example\/quetz\/.*/, command: 'quetz:gotopage'});
 
     commands.addCommand(CommandIDs.open, {
       label: 'Open Logo',
