d9599029c480f34a55c471bc78c80c2b63fe946a
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2021-01-19 09:34:37 +0100

    Update constants.ts

--- src/plugins/channels/constants.ts
+++ src/plugins/channels/constants.ts
@@ -4,5 +4,5 @@ export enum API_STATUSES {
   FAILED = 'FAILED'
 }
 
-export const BACKEND_HOST = 'http://localhost:8000';
-//export const BACKEND_HOST = 'https://beta.mamba.pm';
+//export const BACKEND_HOST = 'http://localhost:8000';
+export const BACKEND_HOST = 'https://beta.mamba.pm';
