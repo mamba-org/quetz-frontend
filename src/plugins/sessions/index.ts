d1a7d441c0928004eaf68dee53aecfa08fc4add1
Carlos Herrero <carlosherrerocontact@gmail.com>
2021-02-02 13:57:23 +0100

    Stop polling models

---
+++ src/plugins/sessions/index.ts
@@ -0,0 +1,35 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * A plugin to open documents in the main area.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'quetz:stop-polling',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd): void => {
+    app.serviceManager.sessions?.ready.then(value => {
+      // stop polling the kernel sessions
+      app.serviceManager.sessions['_kernelManager']['_pollModels']?.stop();
+      // stop polling the sessions
+      void app.serviceManager.sessions['_pollModels'].stop();
+    });
+
+    app.serviceManager.kernelspecs?.ready.then(value => {
+      // stop polling the kernelspecs
+      void app.serviceManager.kernelspecs.dispose();
+    });
+
+    /* 
+    app.serviceManager.terminals?.ready.then( value => {
+      console.debug("Stopping terminals:");
+      // stop polling the terminals
+      void app.serviceManager.terminals['_pollModels'].stop();
+    });
+    */
+  }
+};
+
+export default plugin;
