98504fc3c57caee332d11de739140b05e1c538c5
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-03-25 17:34:40 +0100

    Load extensions in index.js

--- quetz_frontend/app/index.js
+++ quetz_frontend/app/index.js
@@ -3,14 +3,32 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import { App } from '@jupyterlab/application';
+import { App } from '@quetz-frontend/application';
 
 import './style.js';
+
+const extensions = [
+  import('@jupyterlab/apputils-extension'),
+  import('@jupyterlab/theme-light-extension'),
+  import('@jupyterlab/theme-dark-extension'),
+  import('@quetz-frontend/application-extension')
+];
 
 export async function main() {
   const app = new App();
 
-  const mods = [];
-  app.registerPluginModules(mods);
+  const enabled = [
+    '@jupyterlab/apputils-extension:themes',
+    '@jupyterlab/apputils-extension:settings',
+  ];
+  const plugins = (await Promise.all(extensions)).map(mod => {
+    let data = mod.default;
+    if (!Array.isArray(data)) {
+      data = [data];
+    }
+    return data.filter(mod => enabled.includes(mod.id));
+  });
+
+  app.registerPluginModules(plugins);
   await app.start();
 }
