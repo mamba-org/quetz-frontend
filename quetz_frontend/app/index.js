7b5ca6db1f04c7ba2c25f8539bf70c303262604c
Carlos Herrero <carlosherrerocontact@gmail.com>
2021-05-06 19:29:16 +0200

    works

--- quetz_frontend/app/index.js
+++ quetz_frontend/app/index.js
@@ -1,3 +1,4 @@
+// This file is auto-generated from the corresponding file in /dev_mode
 /*-----------------------------------------------------------------------------
 | Copyright (c) Jupyter Development Team.
 | Distributed under the terms of the Modified BSD License.
@@ -5,10 +6,49 @@
 
 import { App } from '@quetz-frontend/application';
 
+import { PageConfig } from '@jupyterlab/coreutils';
+
 import './build/style.js';
 
-async function main() {
+// Promise.allSettled polyfill, until our supported browsers implement it
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+if (Promise.allSettled === undefined) {
+  Promise.allSettled = promises =>
+    Promise.all(
+      promises.map(promise =>
+        promise
+          .then(value => ({
+            status: "fulfilled",
+            value,
+          }), reason => ({
+            status: "rejected",
+            reason,
+          }))
+      )
+    );
+}
+
+async function createModule(scope, module) {
+  try {
+    const factory = await window._JUPYTERLAB[scope].get(module);
+    return factory();
+  } catch(e) {
+    console.warn(`Failed to create module: package: ${scope}; module: ${module}`);
+    throw e;
+  }
+}
+
+export async function main() {
   const app = new App();
+  var disabled = [];
+  var deferred = [];
+  var ignorePlugins = [];
+  var register = [];
+
+
+  const federatedExtensionPromises = [];
+  const federatedMimeExtensionPromises = [];
+  const federatedStylePromises = [];
 
   const mods = [
     require('@jupyterlab/apputils-extension').default.filter(({ id }) =>
@@ -31,8 +71,74 @@ async function main() {
     require('@quetz-frontend/home-extension'),
   ];
 
+  // Start initializing the federated extensions
+  const extensions = JSON.parse(
+    PageConfig.getOption('federated_extensions')
+  );
+
+  const queuedFederated = [];
+
+  extensions.forEach(data => {
+    if (data.extension) {
+      queuedFederated.push(data.name);
+      federatedExtensionPromises.push(createModule(data.name, data.extension));
+    }
+    if (data.mimeExtension) {
+      queuedFederated.push(data.name);
+      federatedMimeExtensionPromises.push(createModule(data.name, data.mimeExtension));
+    }
+    if (data.style) {
+      federatedStylePromises.push(createModule(data.name, data.style));
+    }
+  });
+
+  /**
+   * Iterate over active plugins in an extension.
+   *
+   * #### Notes
+   * This also populates the disabled, deferred, and ignored arrays.
+   */
+  function* activePlugins(extension) {
+    // Handle commonjs or es2015 modules
+    let exports;
+    if (extension.hasOwnProperty('__esModule')) {
+      exports = extension.default;
+    } else {
+      // CommonJS exports.
+      exports = extension;
+    }
+
+    let plugins = Array.isArray(exports) ? exports : [exports];
+    for (let plugin of plugins) {
+      if (PageConfig.Extension.isDisabled(plugin.id)) {
+        disabled.push(plugin.id);
+        continue;
+      }
+      if (PageConfig.Extension.isDeferred(plugin.id)) {
+        deferred.push(plugin.id);
+        ignorePlugins.push(plugin.id);
+      }
+      yield plugin;
+    }
+  }
+
+  // Add the federated extensions.
+  const federatedExtensions = await Promise.allSettled(federatedExtensionPromises);
+  federatedExtensions.forEach(p => {
+    if (p.status === "fulfilled") {
+      for (let plugin of activePlugins(p.value)) {
+        mods.push(plugin);
+      }
+    } else {
+      console.error(p.reason);
+    }
+  });
+
+  // Load all federated component styles and log errors for any that do not
+  (await Promise.allSettled(federatedStylePromises)).filter(({status}) => status === "rejected").forEach(({reason}) => {
+    console.error(reason);
+  });
+
   app.registerPluginModules(mods);
   await app.start();
 }
-
-window.addEventListener('load', main);
--- quetz_frontend/app/webpack.config.js
+++ quetz_frontend/app/webpack.config.js
@@ -2,16 +2,133 @@
 // Distributed under the terms of the Modified BSD License.
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const data = require('./package.json');
 const webpack = require('webpack');
+const { ModuleFederationPlugin } = webpack.container;
 const Build = require('@jupyterlab/builder').Build;
+
+const packageData = require('./package.json');
+const jlab = packageData.jupyterlab;
+
+// Create a list of application extensions and mime extensions from
+// jlab.extensions
+const extensions = {};
+const mimeExtensions = {};
+for (const key of jlab.extensions) {
+  const {
+    jupyterlab: { extension, mimeExtension }
+  } = require(`${key}/package.json`);
+  if (extension !== undefined) {
+    extensions[key] = extension === true ? '' : extension;
+  }
+  if (mimeExtension !== undefined) {
+    mimeExtensions[key] = mimeExtension === true ? '' : mimeExtension;
+  }
+}
 
 // Generate webpack config to copy extension assets to the build directory,
 // such as setting schema files, theme assets, etc.
 const extensionAssetConfig = Build.ensureAssets({
-  packageNames: data.jupyterlab.extensions,
-  output: './build',
+  packageNames: jlab.extensions,
+  output: './build'
 });
+
+/**
+ * Create the webpack ``shared`` configuration
+ */
+function createShared(packageData) {
+  // Set up module federation sharing config
+  const shared = {};
+  const extensionPackages = packageData.jupyterlab.extensions;
+
+  // Make sure any resolutions are shared
+  for (let [pkg, requiredVersion] of Object.entries(packageData.resolutions)) {
+    shared[pkg] = { requiredVersion };
+  }
+
+  // Add any extension packages that are not in resolutions (i.e., installed from npm)
+  for (let pkg of extensionPackages) {
+    if (!shared[pkg]) {
+      shared[pkg] = {
+        requiredVersion: require(`${pkg}/package.json`).version
+      };
+    }
+  }
+
+  // Add dependencies and sharedPackage config from extension packages if they
+  // are not already in the shared config. This means that if there is a
+  // conflict, the resolutions package version is the one that is shared.
+  const extraShared = [];
+  for (let pkg of extensionPackages) {
+    let pkgShared = {};
+    let {
+      dependencies = {},
+      jupyterlab: { sharedPackages = {} } = {}
+    } = require(`${pkg}/package.json`);
+    for (let [dep, requiredVersion] of Object.entries(dependencies)) {
+      if (!shared[dep]) {
+        pkgShared[dep] = { requiredVersion };
+      }
+    }
+
+    // Overwrite automatic dependency sharing with custom sharing config
+    for (let [dep, config] of Object.entries(sharedPackages)) {
+      if (config === false) {
+        delete pkgShared[dep];
+      } else {
+        if ('bundled' in config) {
+          config.import = config.bundled;
+          delete config.bundled;
+        }
+        pkgShared[dep] = config;
+      }
+    }
+    extraShared.push(pkgShared);
+  }
+
+  // Now merge the extra shared config
+  const mergedShare = {};
+  for (let sharedConfig of extraShared) {
+    for (let [pkg, config] of Object.entries(sharedConfig)) {
+      // Do not override the basic share config from resolutions
+      if (shared[pkg]) {
+        continue;
+      }
+
+      // Add if we haven't seen the config before
+      if (!mergedShare[pkg]) {
+        mergedShare[pkg] = config;
+        continue;
+      }
+
+      // Choose between the existing config and this new config. We do not try
+      // to merge configs, which may yield a config no one wants
+      let oldConfig = mergedShare[pkg];
+
+      // if the old one has import: false, use the new one
+      if (oldConfig.import === false) {
+        mergedShare[pkg] = config;
+      }
+    }
+  }
+
+  Object.assign(shared, mergedShare);
+
+  // Transform any file:// requiredVersion to the version number from the
+  // imported package. This assumes (for simplicity) that the version we get
+  // importing was installed from the file.
+  for (let [pkg, { requiredVersion }] of Object.entries(shared)) {
+    if (requiredVersion && requiredVersion.startsWith('file:')) {
+      shared[pkg].requiredVersion = require(`${pkg}/package.json`).version;
+    }
+  }
+
+  // Add singleton package information
+  for (let pkg of packageData.jupyterlab.singletonPackages) {
+    shared[pkg].singleton = true;
+  }
+
+  return shared;
+}
 
 module.exports = [
   {
@@ -19,13 +136,13 @@ module.exports = [
       'whatwg-fetch',
       'react-app-polyfill/ie9', // Only if you want to support IE 9
       'react-app-polyfill/stable',
-      './index.js',
+      './bootstrap.js',
     ],
     output: {
       path: __dirname + '/build',
       filename: 'bundle.js',
     },
-    bail: true,
+    bail: false,
     devtool: 'source-map',
     mode: 'development',
     module: {
@@ -87,6 +204,14 @@ module.exports = [
         template: 'templates/index.ejs',
         inject: false,
       }),
+      new ModuleFederationPlugin({
+        library: {
+          type: 'var',
+          name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION']
+        },
+        name: 'CORE_FEDERATION',
+        shared: createShared(packageData)
+      })
     ],
   },
 ].concat(extensionAssetConfig);
