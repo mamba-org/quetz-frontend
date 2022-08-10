80a51841ca2013b8992b0951de5f7b42864cecb1
Carlos Herrero <contact@carloshb.com>
2022-08-10 20:13:55 +0200

    Adds script to bump the version of the packages and some other fixes for the releaser

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,6 @@
 
 <!-- <START NEW CHANGELOG ENTRY> -->
 
+## 0.3.1
+
 <!-- <END NEW CHANGELOG ENTRY> -->
--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -7,12 +7,13 @@
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
 
+import json
 import click
+from pathlib import Path
 from jupyter_releaser.util import get_version, run
 from pkg_resources import parse_version
 
-LERNA_CMD = "jlpm run lerna version --no-push --force-publish --no-git-tag-version"
-
+LERNA_CMD = "yarn run lerna version --no-push --no-changelog --force-publish --no-git-tag-version"
 
 @click.command()
 @click.option("--force", default=False, is_flag=True)
@@ -53,6 +54,39 @@ def bump(force, spec):
     if force:
         lerna_cmd += " --yes"
     run(lerna_cmd)
+    # update the resolutions in the app package.json
+    _update_resolutions("@quetz-frontend", "~{}".format(js_version))
+
+def _update_resolutions(package, version):
+    """
+        Update the resolutions' version of the app package.json
+        
+        package: prefix of the packages to update.
+                    Example: '@quetz-fronted'
+        version: new version for the packages
+                    Example: '~0.3.2'
+    """
+    HERE = Path(__file__).parent.parent.resolve()
+    file_path = (HERE / "quetz_frontend" / "app" / "package.json")
+
+    if not file_path.exists() :
+        raise FileNotFoundError(f"Could not find package.json under dir {file_path!s}")
+
+    try:
+        with file_path.open('r') as f:
+            pkg_json = json.load(f)
+        
+        resolutions = pkg_json["resolutions"]
+        for pkg in resolutions:
+            if package in pkg:
+                resolutions[pkg] = version
+        
+        pkg_json["resolutions"] = resolutions
+        with file_path.open('w') as f:
+            json.dump(pkg_json, f, indent=2)
+
+    except FileNotFoundError:
+        pass
 
 
 if __name__ == "__main__":
